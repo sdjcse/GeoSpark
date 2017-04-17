fbeec0c1e7cef78bb5b0233ee7fd7bc882f81470
Dhananjayan Santhanakrishnan <sdjcse1@gmail.com>
2017-04-16 17:09:36 -0700

    code cleaning

--- src/main/java/org/datasyslab/geospark/spatialOperator/SpatialStatistics2.java
+++ src/main/java/org/datasyslab/geospark/spatialOperator/SpatialStatistics2.java
@@ -1,6 +1,5 @@
 package org.datasyslab.geospark.spatialOperator;
 
-import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -9,7 +8,6 @@ import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
-import java.io.FileWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -21,9 +19,7 @@ import java.util.*;
 
 public class SpatialStatistics2
 {
-    public static int [][][] cube = new int[40][55][31];
     public static int N = 31 * 55 * 40;
-    public static Map<String, Integer> finValues = new HashMap<>();
     public static Tuple2<String, Integer> getValues(String s)
     {
         String[] dataValue = s.split(",");
@@ -35,7 +31,6 @@ public class SpatialStatistics2
         int day = Integer.parseInt((dataValue[1].split("\\s+|/|-"))[2]);
         String key = y + "," + x + "," + day;
         if (latitude <= 40.9 && latitude >= 40.5 && longitude <= -73.7 && longitude >= -74.25)    {
-            //cube[(int) ((y-40.5)*100)][(int) ((x-73.7)*100)][day-1] = cube[(int) ((y-40.5)*100)][(int) ((x-73.7)*100)][day-1] + 1;
             return new Tuple2<>(key, 1);
         }
         else {
@@ -43,22 +38,22 @@ public class SpatialStatistics2
         }
     }
     private static List<String> getNeighbor(String coord) {
-        String[] coord_array = coord.split(",");
-        List<String> l = new ArrayList<String>();
-        double x1 = Double.parseDouble(coord_array[0]);
-        double y1 = Double.parseDouble(coord_array[1]);
-        int z1 = Integer.parseInt(coord_array[2]);
+        String[] inputParams = coord.split(",");
+        List<String> l = new ArrayList<>();
+        double x1 = Double.parseDouble(inputParams[0]);
+        double y1 = Double.parseDouble(inputParams[1]);
+        int z1 = Integer.parseInt(inputParams[2]);
         l.add((int)x1+","+(int)y1+","+z1);
-        for (int ix = -1; ix <= 1; ix++) {
-            for (int iy = -1; iy <= 1; iy++) {
-                for (int iz = -1; iz <= 1; iz++) {
+        for (int offsetX = -1; offsetX <= 1; offsetX++) {
+            for (int offsetY = -1; offsetY <= 1; offsetY++) {
+                for (int offsetZ = -1; offsetZ <= 1; offsetZ++) {
                     double x = x1;
                     double y = y1;
                     int z = z1;
-                    x = x + ix;
-                    y = y + iy;
-                    z = z + iz;
-                    if (!((ix == 0) && (iy == 0) && (iz == 0))) {
+                    x = x + offsetX;
+                    y = y + offsetY;
+                    z = z + offsetZ;
+                    if (!((offsetX == 0) && (offsetY == 0) && (offsetZ == 0))) {
                         if ((x >= (40.5 * 100d) && x <= (40.9 * 100d))
                                 && (y <= (74.25 * 100d) && y >= (73.7 * 100d))
                                 && (z >= 1 && z <= 31)) {
@@ -67,7 +62,6 @@ public class SpatialStatistics2
                     }
                 }
             }
-
         }
         return l;
     }
@@ -107,13 +101,6 @@ public class SpatialStatistics2
         final Double average = ((double)countTotal)/N;
         System.out.println("Total Count : " + countTotal);
         System.out.println("Average : " + average);
-        /*long flo = redDataMap.values().reduce(new Function2<Integer, Integer, Integer>() {
-            public Integer call(Integer val1, Integer val2) throws Exception {
-                Double st = val2 - average;
-                Double stDev = Math.abs(st) * Math.abs(st);
-                return val1 + (int) Math.round(stDev*100);
-            }
-        });*/
         List<Integer> values = redDataMap.values().collect();
         Double sum2 = 0.0;
         for(Integer iter:values){
@@ -126,37 +113,23 @@ public class SpatialStatistics2
         System.out.println("Standard Deviation : " + stDev);
         System.out.println("Reduced Count = " + redDataMap.count());
         redDataMap = redDataMap.sortByKey();
-        //redDataMap.saveAsHadoopFile(pathFolder, String.class, Integer.class, TextOutputFormat.class);
         Map<String,Double> valueMap = new HashMap<>();
         Map<String,Integer> mapData = redDataMap.collectAsMap();
-       // System.out.println(mapData.size());
         for (Map.Entry<String, Integer> entry : mapData.entrySet())
         {
             List<String> neighbors;
             double sum = 0;
             neighbors = getNeighbor(entry.getKey());
-            //System.out.println(neighbors.size());
             for(String st : neighbors)
             {
                 sum = sum + ((mapData.get(st)!=null)?mapData.get(st):0);
-                //count = count + ((mapData.get(st)!=null)?1:0);
             }
-            //System.out.println(entry.getKey() + " = " + sum);
             double count = neighbors.size();
             double numerator = sum - (average * count);
             double denominator = (stDev * (Math.sqrt((count * N - count * count) / (N - 1))));
-            //double gScore = (cube[i][j][p] - (average * (double) sum)) / (stDev * (Math.sqrt(((double) sum * N - (double) sum * (double) sum) / (N - 1))));
             double gScore = numerator/denominator;
             valueMap.put(entry.getKey(), gScore);
         }
-         /*           double numerator = sum - (average * count);
-                    double denominator = (stDev * (Math.sqrt((count * N - count * count) / (N - 1))));
-                    //double gScore = (cube[i][j][p] - (average * (double) sum)) / (stDev * (Math.sqrt(((double) sum * N - (double) sum * (double) sum) / (N - 1))));
-                    double gScore = numerator/denominator;
-                    valueMap.put(Double.toString(73.7 + (double)j / 100) + ',' + Double.toString(40.5 + (double)p / 100) + ',' + Integer.toString(i), gScore);
-
-          */
-
         int cnt = 0;
         Map<String,Double> sortedMap = sortByComparator(valueMap,false);
         List<String> fileOut = new ArrayList<>();
